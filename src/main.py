0fca0ca2ff3d9d65455040432ed30b5852bc86df
12jihan <jareem.e.hoff@gmail.com>
2026-06-20 16:53:10 -0400

    started adapting the fastapi server

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from kura-ai-service!")
+from fastapi import FastAPI
 
+def main():
+    app = FastAPI()
+
+    @app.get("/"):
+    def read_root():
+        return {"message": "Hello, World! FastAPI is running."}
+
+    @app.get("/items/{item_id}"):
+    def read_item(item_id: int):
+        return {"message": "success", "data": item_id}
 
 if __name__ == "__main__":
     main()
